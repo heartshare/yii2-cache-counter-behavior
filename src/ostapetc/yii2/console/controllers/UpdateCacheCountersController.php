ddde3342c95806f7a2d88b94a49b279cf06c8b42
os <artemostapetc@gmail.com>
2013-11-20 16:22:21 +0400

    add console command

---
+++ src/ostapetc/yii2/console/controllers/UpdateCacheCountersController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: os
+ * Date: 20.11.13
+ * Time: 16:20
+ */
+namespace ostapetc\yii2\console\controllers;
+
+use Yii;
+use yii\console\Exception;
+use yii\console\Controller;
+use yii\db\Connection;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
+
+
+class UpdateCacheCountersController extends Controller
+{
+    public function actionIndex()
+    {
+        echo "Updatecache eee";
+    }
+} 
